b12e91173f4ae645c83aeb8cf302d15d6f71e19e
Jesús González <gopixc@gmail.com>
2014-08-28 12:35:35 -0500

    update controller

--- application/views/layouts/loged.php
+++ application/views/layouts/loged.php
@@ -41,15 +41,23 @@
 				<button type="submit" class="btn btn-inverse">Submit</button>
 			</form>
 			<ul class="nav navbar-nav navbar-right">
-				<li><a href="<?php echo base_url('login'); ?>">Login</a></li>
-				
+				<li><a href="#">Login</a></li>
+				<li class="dropdown">
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <span class="caret"></span></a>
+					<ul class="dropdown-menu" role="menu">
+						<li><a href="#">Action</a></li>
+						<li><a href="#">Another action</a></li>
+						<li><a href="#">Something else here</a></li>
+						<li class="divider"></li>
+						<li><a href="#">Separated link</a></li>
+					</ul>
+				</li>
 			</ul>
 		</div><!-- /.navbar-collapse -->
 	</div><!-- /.container-fluid -->
 </nav>
 
 <div class="container-dn">
-
 	<?php echo $output; ?>
 </div>
 </body>
